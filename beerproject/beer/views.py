9dae3ba959b4ec8299d924cdf318a8b7ddf2e938
2hongbi <foxgirin950@gmail.com>
2022-11-23 11:18:05 +0900

    done with view connection

--- beerproject/beer/views.py
+++ beerproject/beer/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    return render(request, 'beer/index.html')
+
+
+def ver1(request):
+    return render(request, 'beer/ver1.html')
+
+
+def ver2(request):
+    return render(request, 'beer/ver2.html')
--- beerproject/beerproject/settings.py
+++ beerproject/beerproject/settings.py
@@ -104,11 +104,13 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ko-kr'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
+
+USE_L10N = True
 
 USE_TZ = True
 
@@ -116,7 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
