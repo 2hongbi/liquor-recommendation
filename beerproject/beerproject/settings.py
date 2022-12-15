2e715367bc44538666d0dc3a897aa6fe5fd552cb
2hongbi <foxgirin950@gmail.com>
2022-12-16 04:44:16 +0900

    edit settings.py

--- beerproject/beerproject/settings.py
+++ beerproject/beerproject/settings.py
@@ -58,7 +58,6 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -148,7 +147,3 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static_files')
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
